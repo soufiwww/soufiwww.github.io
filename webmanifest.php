0e0e442141b0a8d47bbcc3720e69175c29dac30a
Xanees <86894589+Wolenbyy@users.noreply.github.com>
2022-05-26 16:19:56 +0200

    biggest update
    # 1

---
+++ webmanifest.php
@@ -0,0 +1,24 @@
+{
+    "name": "Legendware",
+    "short_name": "Legendware",
+    "description": "Legendware | Unique products for visual game modification",
+    "icons": [
+        {
+            "src": "\/data\/assets\/logo\/192x192.png",
+            "sizes": "192x192",
+            "purpose": "any maskable"
+        },
+        {
+            "src": "\/data\/assets\/logo\/512x512.png",
+            "sizes": "512x512",
+            "purpose": "any maskable"
+        }
+    ],
+    "lang": "en-US",
+    "dir": "LTR",
+    "display": "minimal-ui",
+    "scope": "\/",
+    "start_url": "\/?_pwa=1",
+    "background_color": "#1a1a1a",
+    "theme_color": "#242424"
+}
